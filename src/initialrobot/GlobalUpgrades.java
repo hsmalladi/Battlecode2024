3a76bf66f2b6ad95a38e9d53c010d13a98a84a02
Colin Zhu <33072296+colinz11@users.noreply.github.com>
2024-01-15 03:34:07 -0500

    fixed buying global upgrades

--- src/initialrobot/GlobalUpgrades.java
+++ src/initialrobot/GlobalUpgrades.java
@@ -6,8 +6,11 @@ public class GlobalUpgrades {
     public static void useGlobalUpgrade(RobotController rc) throws GameActionException {
         if(rc.canBuyGlobal(GlobalUpgrade.ACTION)) {
             rc.buyGlobal(GlobalUpgrade.ACTION);
-        } else if(rc.canBuyGlobal(GlobalUpgrade.HEALING)) {
-            rc.canBuyGlobal(GlobalUpgrade.HEALING);
+            System.out.println("BOUGHT ACTION UPGRADE");
+        }
+        else if(rc.canBuyGlobal(GlobalUpgrade.HEALING)) {
+            rc.buyGlobal(GlobalUpgrade.HEALING);
+            System.out.println("BOUGHT HEALING UPGRADE");
         }
     }
 }

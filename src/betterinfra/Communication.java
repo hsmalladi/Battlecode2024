1dd2cb2894ccf2c84bd498dbc793f4c6ef3dc3dc
Havish Malladi <havish.malladi@duke.edu>
2024-01-16 14:18:59 -0500

    new stuff

---
+++ src/betterinfra/Communication.java
@@ -0,0 +1,6 @@
+package betterinfra;
+
+public class Communication {
+    final static int FLAG_COMM = 0;
+    final static int EXPLORER_COMM = 1;
+}
---
+++ src/betterinfra/Util.java
@@ -0,0 +1,9 @@
+package betterinfra;
+
+
+public class Util {
+    static boolean hurt(int h){
+        return h < Constants.CRITICAL_HEALTH;
+    }
+
+}

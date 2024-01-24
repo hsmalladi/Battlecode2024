ff7c18b3a990cb4eb143be0fd2cf2f06c22cf1bf
louishu17 <lh329@duke.edu>
2024-01-23 19:58:39 -0500

    switch order of global upgrades

--- src/betterinfra/GlobalUpgrades.java
+++ src/betterinfra/GlobalUpgrades.java
@@ -10,13 +10,13 @@ public class GlobalUpgrades extends Globals {
             rc.buyGlobal(GlobalUpgrade.ATTACK);
             System.out.println("BOUGHT ATTACK UPGRADE");
         }
-        else if(rc.canBuyGlobal(GlobalUpgrade.CAPTURING)) {
-            rc.buyGlobal(GlobalUpgrade.CAPTURING);
-            System.out.println("BOUGHT CAPTURING UPGRADE");
-        }
         else if(rc.canBuyGlobal(GlobalUpgrade.HEALING)) {
             rc.buyGlobal(GlobalUpgrade.HEALING);
             System.out.println("BOUGHT HEALING UPGRADE");
+        }
+        else if(rc.canBuyGlobal(GlobalUpgrade.CAPTURING)) {
+            rc.buyGlobal(GlobalUpgrade.CAPTURING);
+            System.out.println("BOUGHT CAPTURING UPGRADE");
         }
     }
 }

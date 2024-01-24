f781cae6fb84a82e9e4d37bb17f0b38b33284dfb
Colin Zhu <33072296+colinz11@users.noreply.github.com>
2024-01-24 17:25:23 -0500

    clear comms + lv6 builders

--- src/smurf/BotMainRoundDuck.java
+++ src/smurf/BotMainRoundDuck.java
@@ -1,9 +1,6 @@
 package smurf;
 
-import battlecode.common.FlagInfo;
-import battlecode.common.GameActionException;
-import battlecode.common.GameConstants;
-import battlecode.common.MapLocation;
+import battlecode.common.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,21 +8,25 @@ import java.util.List;
 public class BotMainRoundDuck extends BotDuck {
 
     private static final int
-                    ATTACK_DUCK = 1,
-                    FLAG_CARRIER = 2,
-                    FLAG_DEFENSE_DUCK = 3;
+            ATTACK_DUCK = 1,
+            FLAG_CARRIER = 2,
+            FLAG_DEFENSE_DUCK = 3;
 
     private static int myRole = -1;
 
-    public static int myFlag = -1;
+    public static int myFlagHolding = -1;
+    static boolean amHoldingFlag = false;
+    private static int roundDied = -1;
+
 
     public static boolean goingToFlag = true;
 
     public static void play() throws GameActionException {
         GlobalUpgrades.useGlobalUpgrade();
+        tryFlagDropOff();
         if (!rc.isSpawned()) {
             if (flagDuck != 0) {
-                System.out.println("DIED ENEMY PROLLY HAS OUR FLAG" + rc.readSharedArray(flagDuck+50));
+                //System.out.println("DIED ENEMY PROLLY HAS OUR FLAG" + rc.readSharedArray(flagDuck+50));
                 rc.writeSharedArray(flagDuck + 50, rc.readSharedArray(flagDuck + 50) +  1);
             }
             smartSpawn();
@@ -58,15 +59,38 @@ public class BotMainRoundDuck extends BotDuck {
         }
     }
 
-
+    private static void tryFlagDropOff() throws GameActionException {
+        try {
+            if (!rc.isSpawned() && amHoldingFlag && !rc.hasFlag()) {
+                amHoldingFlag = false;
+                goingToFlag = true;
+                roundDied = rc.getRoundNum();
+                System.out.println("I DIED HOLDING FLAG");
+            }
+            else if (!rc.hasFlag() && amHoldingFlag) {
+                amHoldingFlag = false;
+                goingToFlag = true;
+                Comm.updateFlagInfo(null, false, myFlagHolding);
+                System.out.println("DROPPED OFF FLAG " + myFlagHolding);
+            }
+            if (rc.getRoundNum() == roundDied + 4) {
+                Comm.updateFlagInfo(null, false, myFlagHolding);
+                System.out.println("RESETTING FLAG LOCATION");
+            }
+        } catch (Exception e) {
+            System.out.println("HELLO HELLO HELLO HELLO");
+        }
+    }
 
 
 
     private static void tryFlagPickUp() throws GameActionException {
         for (FlagInfo loc : rc.senseNearbyFlags(GameConstants.VISION_RADIUS_SQUARED)) {
             if (rc.canPickupFlag(loc.getLocation())) {
+                myFlagHolding = Comm.flagIDToIdx(loc.getID(), rc.getTeam().opponent());
                 rc.pickupFlag(loc.getLocation());
                 goingToFlag = false;
+                amHoldingFlag = true;
                 rc.writeSharedArray(Comm.ENEMY_FLAG_HELD, 1);
                 break;
             }

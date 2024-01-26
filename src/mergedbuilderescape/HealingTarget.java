fe177718539fc01f0150eeea7a733dddd080d3d3
Colin Zhu <33072296+colinz11@users.noreply.github.com>
2024-01-26 12:29:31 -0500

    added level tie breaker for targets

--- src/mergedbuilderescape/HealingTarget.java
+++ src/mergedbuilderescape/HealingTarget.java
@@ -4,20 +4,38 @@ import battlecode.common.MapLocation;
 import battlecode.common.RobotInfo;
 
 public class HealingTarget extends Globals {
-    int health;
+    int health, attackLvl, healLvl, buildLvl, maxLvl, score;
     boolean flagHolder = false;
     MapLocation mloc;
 
-    boolean isBetterThan(HealingTarget t){
+    int maxLevel(int attack, int heal, int build) {
+        return Math.max(attack, Math.max(heal, build));
+    }
+
+    int getScore(int maxLvl, int hp) { //higher scoring ducks are healed
+        return maxLvl * 100 + (800 - hp);
+    }
+
+    boolean isBetterThan(HealingTarget t) {
         if (t == null) return true;
         if (flagHolder && !t.flagHolder) return true;
         if (!flagHolder && t.flagHolder) return false;
-        return health <= t.health;
+        if (health <= t.health) return true;
+        if (maxLvl > t.maxLvl) return true;
+//        if (score > t.score) return true;
+        return false;
     }
+
+
 
     HealingTarget(RobotInfo r){
         health = r.getHealth();
         mloc = r.getLocation();
         flagHolder = r.hasFlag();
+        attackLvl = r.getAttackLevel() + 2; // weigh attack ducks heavier than build/heal
+        buildLvl = r.getBuildLevel();
+        healLvl = r.getHealLevel() + 1;
+        maxLvl = maxLevel(attackLvl, healLvl, buildLvl);
+        score = getScore(maxLvl, health);
     }
 }

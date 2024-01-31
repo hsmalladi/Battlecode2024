f4899996e3699b3f145171059a2242975f2b73f8
Colin Zhu <33072296+colinz11@users.noreply.github.com>
2024-01-31 15:15:30 -0500

    usquals2

---
+++ src/usquals2/Debug.java
@@ -0,0 +1,26 @@
+package usquals2;
+
+public class Debug extends Globals {
+
+    public static boolean DEBUGGING = false;
+
+
+    public static void log(String s) {
+        if (DEBUGGING)
+            System.out.println(s);
+    }
+
+    public static void log(int robotID, String s) {
+        if (DEBUGGING) {
+            System.out.println(String.format("Robot %d: %s", robotID, s));
+        }
+    }
+
+    public static void log(int robotID, int roundNum, String s) {
+        if (DEBUGGING) {
+            System.out.println(String.format("Robot %d (Round %d): %s", robotID, roundNum, s));
+        }
+    }
+
+
+}
---
+++ src/usquals2/Util.java
@@ -0,0 +1,8 @@
+package usquals2;
+
+public class Util {
+    static boolean hurt(int h){
+        return h < Constants.CRITICAL_HEALTH;
+    }
+
+}
